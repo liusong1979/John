251588c629064948df8cc08a96599c547b20d51e
Bios <bios@Bios.local>
2016-10-06 14:50:22 -0400

    2nd version of exercise

--- R/hi.R
+++ R/hi.R
@@ -1,17 +1,22 @@
 #'Help statrt conversions
-#' 
+#'
 #'This function is for greeting in different ways
-#' 
-#'@param who character(1) The name of the person to greet at 
-#'  
+#'
+#'@param who character(1) The name of the person to greet at
+#'
+#'@param how character(1) The name of the person to convert
+#'
 #'@return a line of the text to be used for starting the conversion
-#'  
-#'@examples 
+#'
+#'@examples
 #'    hi("Song Liu")
-#'  
+#'
 #'@export
 
 hi <- function(who){
-  
-  paste("Hello", who, ", your name is", who, "!")
+  paste("Hello", who, ", your name is", how(who), "!")
+}
+
+how <- function(who){
+  toupper(who)
 }
